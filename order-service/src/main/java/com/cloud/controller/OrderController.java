0f5cb2a5d406d8ccd5d9323b66bcc4ed5436dd69
duyphuong205 <phuongduy220502@gmail.com>
2024-02-04 17:07:15 +0700

    init inventory

--- order-service/src/main/java/com/cloud/controller/OrderController.java
+++ order-service/src/main/java/com/cloud/controller/OrderController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @PostMapping
     public ResponseEntity<?> doCreate(@RequestBody OrderRequest orderRequest) {
         try {
             log.info("----------CREATE ORDER------------");
